1dbc10f42b4f6191dbfc03fcae2ec0b04802795d
Ivo Dancet <ivo.dancet@gmail.com>
2012-09-06 10:54:15 +0200

    poging om alles ouder dan ie 9 uit te schakelen

--- public/_javascript/application.js
+++ public/_javascript/application.js
@@ -1,4 +1,9 @@
 jQuery(function($){
+  if($.browser.msie == true){
+    if(parseInt($.browser.version, 10) < 9){
+      window.location.replace("https://docs.google.com/document/d/1o6jimb5sxTywY_zS1TUwV8JhguE54pB8eXVbHtr7g2c/edit");
+    }
+  }
   $("#twitter").tweet({
     // avatar_size: 32,
     count: 3,
